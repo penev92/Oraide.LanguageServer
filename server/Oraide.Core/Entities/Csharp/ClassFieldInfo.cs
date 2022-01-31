ec2dd20e3b6f29827627855161c3a7a358713d49
penev92 <penev92@gmail.com>
2022-01-31 11:43:19 +0200

    Started with the structs

---
+++ server/Oraide.Core/Entities/Csharp/ClassFieldInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace Oraide.Core.Entities.Csharp
+{
+	/// <summary>
+	/// Represents information about a C# class field.
+	/// </summary>
+	public readonly struct ClassFieldInfo
+	{
+		public string PropertyName { get; }
+
+		public string PropertyType { get; }
+
+		public string DefaultValue { get; }
+
+		public MemberLocation Location { get; }
+
+		public string Description { get; }
+
+		public string OtherAttribute { get; }
+
+		public ClassFieldInfo(string propertyName, string propertyType, string defaultValue, MemberLocation location, string description, string otherAttribute)
+		{
+			PropertyName = propertyName;
+			PropertyType = propertyType;
+			DefaultValue = defaultValue;
+			Location = location;
+			Description = description;
+			OtherAttribute = otherAttribute;
+		}
+	}
+}
--- server/Oraide.Core/Entities/Csharp/TraitInfo.cs
+++ server/Oraide.Core/Entities/Csharp/TraitInfo.cs
@@ -1,30 +1,8 @@
 ﻿namespace Oraide.Core.Entities.Csharp
 {
-	public readonly struct TraitPropertyInfo
-	{
-		public string PropertyName { get; }
-
-		public string PropertyType { get; }
-
-		public string DefaultValue { get; }
-
-		public MemberLocation Location { get; }
-
-		public string Description { get; }
-
-		public string OtherAttribute { get; }
-
-		public TraitPropertyInfo(string propertyName, string propertyType, string defaultValue, MemberLocation location, string description, string otherAttribute)
-		{
-			PropertyName = propertyName;
-			PropertyType = propertyType;
-			DefaultValue = defaultValue;
-			Location = location;
-			Description = description;
-			OtherAttribute = otherAttribute;
-		}
-	}
-
+	/// <summary>
+	/// Represents information about a TraitInfo (the OpenRA abstract class).
+	/// </summary>
 	public readonly struct TraitInfo
 	{
 		public string TraitName { get; }
