95cd0df04a96637ae0ff94f720b4183d73a921b7
penev92 <penev92@gmail.com>
2022-02-02 19:47:52 +0200

    Improved C# class field hover info markdown

--- server/Oraide.Core/Entities/Csharp/ClassFieldInfo.cs
+++ server/Oraide.Core/Entities/Csharp/ClassFieldInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace Oraide.Core.Entities.Csharp
+﻿using System.Linq;
+
+namespace Oraide.Core.Entities.Csharp
 {
 	/// <summary>
 	/// Represents information about a C# class field.
@@ -29,9 +31,24 @@
 
 		public override string ToString() => $"{Type} {Name}";
 
-		public string ToMarkdownInfoString() => "```csharp\n" +
-		                                $"{Name} ({Type})" +
-		                                $"\n```\n" +
-		                                $"{Description}\n\nDefault value: {DefaultValue}";
+		public string ToMarkdownInfoString()
+		{
+			var content = "```csharp\n" +
+			              $"{Name}" + (string.IsNullOrWhiteSpace(Type) ? "" : $" ({Type})") +
+			              "\n```\n" +
+			              $"{Description}";
+
+			if (!string.IsNullOrWhiteSpace(DefaultValue))
+				content += $"\n\nDefault value: {DefaultValue}";
+
+			if (OtherAttributes.Length != 0)
+				content += "\n" +
+				           "```yaml\n\n" +
+						   "Field Attributes\n" +
+				           $"{string.Join("\n", OtherAttributes.Select(x => $"  {x.Name}: {x.Value}"))}" +
+				           "\n```";
+
+			return content;
+		}
 	}
 }
