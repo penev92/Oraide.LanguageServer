fb523c107840e6d527065b38b291ef81270532a3
penev92 <penev92@gmail.com>
2022-04-25 01:30:30 +0300

    ICodeSymbolGenerationStrategy

---
+++ server/Oraide.Csharp/CodeSymbolGenerationStrategies/CodeParsingSymbolGenerationStrategy.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Oraide.Core.Entities.Csharp;
+using Oraide.Csharp.CodeParsers;
+
+namespace Oraide.Csharp.CodeSymbolGenerationStrategies
+{
+	class CodeParsingSymbolGenerationStrategy : ICodeSymbolGenerationStrategy
+	{
+		private readonly string openRaFolder;
+
+		ILookup<string, TraitInfo> traitInfos;
+		WeaponInfo weaponInfo;
+
+		public CodeParsingSymbolGenerationStrategy(string openRaFolder)
+		{
+			this.openRaFolder = openRaFolder;
+		}
+
+		public ILookup<string, TraitInfo> GetTraitInfos()
+		{
+			if (traitInfos == null)
+				Parse();
+
+			return traitInfos;
+		}
+
+		public WeaponInfo GetWeaponInfo()
+		{
+			if (weaponInfo.WeaponPropertyInfos == null)
+				Parse();
+
+			return weaponInfo;
+		}
+
+		void Parse()
+		{
+			var (traits, weapons) = RoslynCodeParser.Parse(openRaFolder);
+			traitInfos = traits;
+			weaponInfo = weapons;
+		}
+	}
+}
---
+++ server/Oraide.Csharp/CodeSymbolGenerationStrategies/FromStaticFileSymbolGenerationStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Oraide.Core.Entities.Csharp;
+
+namespace Oraide.Csharp.CodeSymbolGenerationStrategies
+{
+	class FromStaticFileSymbolGenerationStrategy : ICodeSymbolGenerationStrategy
+	{
+		public ILookup<string, TraitInfo> GetTraitInfos()
+		{
+			throw new NotImplementedException();
+		}
+
+		public WeaponInfo GetWeaponInfo()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
